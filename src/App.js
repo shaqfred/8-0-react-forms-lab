8724fa226aac7c2ecbfc0b1567822af76b70d413
Karolin Rafalski <kr2174@caa.columbia.edu>
2022-10-07 11:50:32 -0400

    update to functional components add config return for plugin

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React from "react";
 import Form from "./Form";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <main>
-        <p>Enter each number in the array, separated by a ','</p>
-        <Form />
-        <section id="result">
-          <p></p>
-        </section>
-      </main>
-    );
-  }
+function App() {
+  return (
+    <main>
+      <p>Enter each number in the array, separated by a ','</p>
+      <Form />
+      <section id="result">
+        <p></p>
+      </section>
+    </main>
+  );
 }
 
 export default App;
