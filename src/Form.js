3efef2d1ce7b6b70ed048880a284409c61669a67
shaquala <shaqualafredericks@pursuit.org>
2022-10-16 22:50:23 -0400

    I was able to fix the errors

--- src/Form.js
+++ src/Form.js
@@ -1,3 +1,4 @@
+import { toHaveAccessibleDescription } from "@testing-library/jest-dom/dist/matchers";
 import { render } from "@testing-library/react";
 import React from "react";
 import "./Form.css";
@@ -6,152 +7,99 @@ import "./Form.css";
 function Form() {
   return (
 
-    findMode =(numArr) => {
-      let obj = {};
-      numArr.forEach((num)=>{
-        if(obj.hasOwnProperty((num)) {
-          obj[num] += 1;
-        } else {
-          obj[num] = 1;
-        }
-        });
-      })
-
-    //   let newArr = Objects.entries(obj);
-    //   let mode = newArr[0]
-    //   for (let i = 1, i < newArr.length; i++) {
-    //     if (newArr[i][1] > mode [1]) {
-    //       mode = newArr[i];
-    //     }
-    //   }
-    //   return mode [0];
-     };
-    handleTextInput =(event)=> {
-      const {value} = event.target;
-      const numArr =value.split(',').map((ele) => {
-        return Number(ele);
-      });
-      this.setState({
-        numbers: numArr,
-        inputValue: value,
-      });
-    };
-
-    handleTextInput = (event) => {
-      const{value} = event.target;
-      this.setState({
-        operation: value,
-      });
-    };
-
-    inputValidator = ()=> {
-      const{ numbers, operation} = this.state;
-      if (numbers.length === 0) {
-        return false;
-      
-      } else if (
-        numbers.some((number)=> {
-          return Number.isNaN(number);
-        })
-    
-
-    ){
-return false;
-
-
-    }
-     else if (
-      operation !== 'sum' &&
-      operation !== 'average' &&
-      operation !-- 'mode'
-
-     
-    } ;
-      return false;
-  
-    return true;
-
-
-handleFormSubmit =(event)=> {
-  event.preventDefault();
-  const {numbers, operation} = this.state;
-
-  if (!this.inputValidator() {
-    this.setState({ result: 'Invalid input.',errorTest: 'error'});
-  }else{
-    if(operation === 'sum'){
-      this.setState({
-        result: numbers.reduce((acc, ele) => {
-          return (acc += ele);
-        }),
-        inputValue: '',
-        operation:'',
-        errorTest: 'noError',
- });
-    }else if (operation === 'average') {
-      this.setState({
-        result: (
-          numbers.reduce((acc, ele) => {
-            return acc + ele;
-
-          }) / numbers.length
-        ).toFixed(2),
-         inputValue: '',
-         operation: '',
-         errorTest: 'noError',
-          
-      });
-    }else{
-      this.setState({
-        result: this.findMode(numbers),
-        inputValue: '',
-        operation: '',
-        errorTest: 'noError',
-
-      });
-    }
-
-    }
-  }
-
-
-render() ;
-  const {result,inputValue,operation,errorTest} = this.state;
-  return
-
-
     <form>
-      <input id="values" name="values" type="text" />
-      <select id="operation" name="operation">
-        <option value=""></option>
-        <option value="sum">sum</option>
-        <option value="average">average</option>
-        <option value="mode">mode</option>
-      </select>
-      <button type="submit">Calculate</button>
-    
+    <input id="values" name="values" type="text" />
+    <select id="operation" name="operation">
+      <option value=""></option>
+      <option value="sum">sum</option>
+      <option value="average">average</option>
+      <option value="mode">mode</option>
+    </select>
+    <button type="submit">Calculate</button>
+  </form>
+  );
+
+constructor() 
+  super();
+
+  this.state = {
+    operation: "",
+    result: "",
+    nums: "",
+  };
+}
+
+
+
+
+handleSubmit = (event) => {
+  event.preventDefault();
+  let numArr = this.state.nums.split(",").map((num)=> Number(num));
+  if(this.state.operation && this.state.nums) {
+    if(this.state.operation === "sum"){
+      this.setState({
+        result: numArr.reduce((num, next) => {
+          return num + next;
+        }, 0),
+        });
+      }else if(this.state.operation === "mode") {
+        let frequency = [];
+        numArr.forEach((num)=> {
+          frequency[num] ? (frequency[num] += 1) : (frequency[num] = 1);
+        });
+        let mode =numArr[0];
+        for(let i = 0; i < frequency.length; i++){
+          if(frequency[i] > frequency [mode]) {
+            mode = i
+          }
+        }
+        this.setState({result: mode});
+
+      }else if (this.state.operation === "average"){
+        this.setState({
+          result:
+          numArr.reduce((num, next)=>{
+            return num + next;
+          }, 0) / numArr.length,
+          });
+        }
+      }else{
+        this.setState({result: "Invalid input"});
+      }
+    };
+    handleOperation = (event) => {
+      const{value} = event.target;
+      this.setState({operation: value});
+    };
+  handleNums = (event) => {
+    const{value} = event.target;
+    this.setState({nums: value});
+  };
+
   <div>
-    <form onSubmit={this.handleFormSubmit}>
+    <form onSubmit={this.handleSubmit}>
       <input
       id='values'
       name='values'
       type='text'
-      value={inputValue}
-      onChange={this.handleTextInput}
-      class={errorTest}
+      value={nums}
+      onChange={this.handleNums}
+     
       />
       <select
       id='operation'
       name='operation'
       value={operation}
-      onChange={this.handleTextInput}
-      class={errorTest}
+      onChange={this.handleOperation}
+     
       ></select>
        
      
     </form>
     <p>{result}</p>
   </div>
-  
-/</form>
+ 
+     
+
+
 export default Form;
